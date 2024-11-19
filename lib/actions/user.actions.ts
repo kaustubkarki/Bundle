5a971a37ebc362cda8ac4dd633a07526739104a2
Kaustub karki <kaustubkarki@gmail.com>
2024-11-19 19:20:52 +0545

    OTP == true

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { createAdminClient } from "@/lib/appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { ID, Query } from "node-appwrite";
+import { parseStringify } from "../utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+export const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://img.freepik.com/premium-vector/man-empty-avatar-casual-business-style-vector-photo-placeholder-social-networks-resumes_885953-434.jpg",
+        accountId,
+      }
+    );
+  }
+  return parseStringify({ accountId });
+};
---
+++ lib/appwrite/index.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+//! Initializes the Appwrite Client and configures it with the endpointUrl and projectId from appwriteConfig.
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  //!Fetches the appwrite-session cookie from the incoming request using next/headers.
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) throw new Error("No session");
+
+  client.setSession(session.value);
+
+  //! returns an object with convenient getters for interacting with Appwrite's Account and Databases services.
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+//!Initializes the Appwrite Client and configures it with the endpointUrl and projectId from appwriteConfig
+export const createAdminClient = async () => {
+  //!Authenticates the client with an API key using client.setKey(appwriteConfig.secretKey).
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  //!Returns an object with getters for interacting with Appwrite's Account, Databases, Storage, and Avatars services.
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
+//

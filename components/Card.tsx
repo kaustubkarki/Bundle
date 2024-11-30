a67a0b7b83829bf7dea8843972db91a96adcf39f
Kaustub karki <kaustubkarki@gmail.com>
2024-11-30 21:33:54 +0545

    actionButton

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,24 +7,27 @@ import ActionDropDown from "./ActionDropDown";
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
-    <Link href={file.url} target="_blank" className="file-card">
+    <div className="file-card">
       <div className="flex justify-between">
-        <Thumbnail
-          type={file.type}
-          extension={file.extension}
-          url={file.url}
-          className="!size-20"
-          imageClassName="!size-11"
-        />
-
+        <Link href={file.url} target="_blank">
+          <Thumbnail
+            type={file.type}
+            extension={file.extension}
+            url={file.url}
+            className="!size-20"
+            imageClassName="!size-11"
+          />
+        </Link>
         <div className="flex flex-col items-end justify-between">
-          <ActionDropDown />
+          <ActionDropDown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
 
       <div className="file-card-details">
-        <p className="subtitle-2 line-clamp-1">{file.name}</p>
+        <Link href={file.url} target="_blank">
+          <p className="subtitle-2 line-clamp-1">{file.name}</p>
+        </Link>
         <FormattedDateTime
           date={file.$createdAt}
           className="body-2 text-light-100"
@@ -33,7 +36,7 @@ const Card = ({ file }: { file: Models.Document }) => {
           By: {file.owner.fullName}
         </p>
       </div>
-    </Link>
+    </div>
   );
 };
 export default Card;
